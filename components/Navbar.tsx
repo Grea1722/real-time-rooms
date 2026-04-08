040433c3a2c3b9e5fdd186dc86b8a61cd5a97cc4
Grea1722 <alexs17@live.com.mx>
2026-04-07 21:55:41 -0700

    Ponte a programar coconut

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,6 +12,8 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between p-4 bg-primary text-white'>
         <input type="text" placeholder='Buscar habitacion, huesped o estado' className='px-4 py-2 rounded-md bg-secondary text-white focus:outline-none focus:ring-2 focus:ring-secondary' />
+        <p className="text-red-500">Ponte a aprender a programar pinche coco ya me tienes hasta la madre, pendejo estupido idiota imbecil</p>
+        <h1 className="text-pink-200">TQM Thanos</h1>
         <h2>{cleanName}</h2>
     </div>
   )
